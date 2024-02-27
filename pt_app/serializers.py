1305ba91ddee143b583c1910dc04bc51929aac70
jhirschv <johnhirschv@gmail.com>
2024-02-26 22:54:50 -0500

    got program update api endpoint to work

--- pt_app/serializers.py
+++ pt_app/serializers.py
@@ -21,21 +21,57 @@ class ExerciseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class WorkoutExerciseSerializer(serializers.ModelSerializer):
-    # Explicitly serialize the related Exercise using ExerciseSerializer
     exercise = ExerciseSerializer(read_only=True)
-    
+    exercise_id = serializers.PrimaryKeyRelatedField(
+        queryset=Exercise.objects.all(), source='exercise', write_only=True
+    )
+
     class Meta:
         model = WorkoutExercise
-        fields = '__all__'
+        fields = '__all__'  # Make sure to include 'exercise' and 'exercise_id'
+
+    def create(self, validated_data):
+        # This method might be adjusted based on your exact requirements
+        return WorkoutExercise.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # Similarly, adjust this method as needed
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 class WorkoutSerializer(serializers.ModelSerializer):
-    # Serialize nested WorkoutExercises within each Workout
-    workout_exercises = WorkoutExerciseSerializer(many=True, read_only=True)
+    workout_exercises = WorkoutExerciseSerializer(many=True)
 
     class Meta:
         model = Workout
         fields = '__all__'
 
+    def create(self, validated_data):
+        workout_exercises_data = validated_data.pop('workout_exercises')
+        workout = Workout.objects.create(**validated_data)
+        for workout_exercise_data in workout_exercises_data:
+            WorkoutExercise.objects.create(workout=workout, **workout_exercise_data)
+        return workout
+
+    def update(self, instance, validated_data):
+        workout_exercises_data = validated_data.pop('workout_exercises')
+        # Update the workout instance
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Handle updating or creating workout exercises
+        for workout_exercise_data in workout_exercises_data:
+            exercise_id = workout_exercise_data.get('exercise').id
+            workout_exercise_instance, created = WorkoutExercise.objects.update_or_create(
+                workout=instance, exercise_id=exercise_id,
+                defaults=workout_exercise_data
+            )
+
+        return instance
+    
 class PhaseSerializer(serializers.ModelSerializer):
     # Serialize nested Workouts within each Phase
     workouts = WorkoutSerializer(many=True, read_only=True)
