ff0a54043ade94e5770a584f29c28f14aef5b05f
jhirschv <johnhirschv@gmail.com>
2024-05-03 16:17:50 -0400

    added backend registation validation

--- pt_app/serializers.py
+++ pt_app/serializers.py
@@ -4,15 +4,50 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.db.models import Max
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator, MinLengthValidator, MaxLengthValidator
 
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    username = serializers.CharField(
+        validators=[
+            RegexValidator(
+                regex='^[a-zA-Z0-9_]+$',
+                message='Username must be alphanumeric and can include underscores',
+                code='invalid_username'
+            ),
+            MinLengthValidator(4, message="Username must be at least 4 characters long."),
+            MaxLengthValidator(20, message="Username must be no longer than 20 characters.")
+        ]
+    )
+    password = serializers.CharField(
+        write_only=True, 
+        required=True, 
+        style={'input_type': 'password'},
+        validators=[
+            RegexValidator(
+                regex='^[a-zA-Z0-9!@#$%^&*()_+=\[\]{};:\'"\\|,.<>\/?~-]+$',
+                message="Password must consist of alphanumeric and special characters only."
+            ),
+            MinLengthValidator(8, message="Password must be at least 8 characters long."),
+            MaxLengthValidator(20, message="Password must be no longer than 20 characters.")
+        ]
+    )
+    email = serializers.EmailField()
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')  # You can add more fields if needed
+        fields = ('username', 'password', 'email')
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("A user with that username already exists.")
+        return value
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with that email already exists.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(
