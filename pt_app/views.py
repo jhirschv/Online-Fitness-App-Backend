d9ee7d4413aee2366bb8d14b1d65d927eabaa5a4
jhirschv <johnhirschv@gmail.com>
2024-05-22 15:12:30 -0400

    added video render, upload, and delete in the progress calendar

--- pt_app/views.py
+++ pt_app/views.py
@@ -322,7 +322,7 @@ class VideoUploadAPI(APIView):
             serializer = ExerciseSetVideoSerializer(exercise_set, data=request.data, partial=True)
             if serializer.is_valid():
                 serializer.save()
-                return Response({'message': 'Video uploaded successfully', 'status': 'success'}, status=status.HTTP_200_OK)
+                return Response(serializer.data, status=status.HTTP_200_OK)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except ExerciseSet.DoesNotExist:
